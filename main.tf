ca9d29040f761d5494470547aa7eb6e65315da55
prakashvelusamy-hcl <prakashvelusamy1999@gmail.com>
2025-07-08 21:26:32 +0530

    eks-fix

---
+++ main.tf
@@ -0,0 +1,92 @@
+module "vpc" {
+  source         = "./modules/terraform-aws-vpc"
+  vpc_cidr       = var.vpc_cidr
+  pub_sub_count  = var.pub_sub_count
+  priv_sub_count = var.priv_sub_count
+  nat_count      = var.nat_count
+}
+# module "alb" {
+#   source = "./modules/terraform-aws-alb"
+#   public_subnet_ids = module.vpc.public_subnet_ids
+#   environment = var.environment
+#   vpc_id = module.vpc.vpc_id
+# }
+
+module "eks" {
+  source                      = "./modules/terraform-aws-eks"
+  private_subnet_ids          = module.vpc.private_subnet_ids
+  public_subnet_ids           = module.vpc.public_subnet_ids
+  cluster_role_arn            = module.iam.eks_cluster_role_arn
+  node_role_arn               = module.iam.eks_node_role_arn
+  eks_oidc_root_ca_thumbprint = var.eks_oidc_root_ca_thumbprint
+  cluster_role_dependency     = module.iam.eks_role_depends_on
+  vpc_id                      = module.vpc.vpc_id
+  credentials_secret_arn      = module.rds.credentials_secret_arn
+  connection_secret_arn       = module.rds.connection_secret_arn
+  providers = {
+    kubernetes = kubernetes.eks
+  }
+  depends_on = [module.vpc]
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+# provider "kubernetes" {
+#   host                   = module.eks.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+# }
+
+module "rds" {
+  source             = "./modules/terraform-aws-rds"
+  private_subnet_ids = module.vpc.private_subnet_ids
+  vpc_id             = module.vpc.vpc_id
+  eks_node_sg_id     = module.eks.eks_node_sg_id
+
+}
+
+#module "apps" {
+#  source = "./modules/terraform-aws-apps"
+#}
+
+
+module "iam" {
+  source                                           = "./modules/terraform-aws-iam"
+  aws_iam_openid_connect_provider_arn              = module.eks.aws_iam_openid_connect_provider_arn
+  aws_iam_openid_connect_provider_extract_from_arn = module.eks.aws_iam_openid_connect_provider_extract_from_arn
+}
+
+
+module "helm" {
+  source                             = "./modules/terraform-aws-helm"
+  cluster_id                         = module.eks.cluster_id
+  cluster_endpoint                   = module.eks.cluster_endpoint
+  cluster_certificate_authority_data = module.eks.cluster_certificate_authority_data
+  lbc_iam_depends_on                 = module.iam.lbc_iam_depends_on
+  lbc_iam_role_arn                   = module.iam.lbc_iam_role_arn
+  vpc_id                             = module.vpc.vpc_id
+  region                             = var.region
+
+  providers = {
+    kubernetes = kubernetes.eks
+    helm       = helm.eks
+  }
+}
+
+provider "kubernetes" {
+  alias                  = "eks"
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  alias = "eks"
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
---
+++ modules/terraform-aws-eks/main.tf
@@ -0,0 +1,122 @@
+resource "aws_eks_cluster" "eks" {
+  name     = "EKS"
+  role_arn = var.cluster_role_arn
+  version  = "1.32"
+  vpc_config {
+    subnet_ids = var.private_subnet_ids
+  }
+}
+
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+
+# resource "aws_launch_template" "eks_node_lt" {
+#   name_prefix   = "eks-node-"
+#   image_id      = "ami-0f4ea56cce2a654c6"
+#   instance_type = "t3.medium"
+
+#   vpc_security_group_ids = [aws_security_group.eks.id]
+
+#   tag_specifications {
+#     resource_type = "instance"
+
+#     tags = {
+#       "Name" = "eks-node"
+#     }
+#   }
+# }
+
+
+# resource "aws_eks_node_group" "ng" {
+#   cluster_name    = aws_eks_cluster.eks.name
+#   node_group_name = "node_group_flask"
+#   node_role_arn   = var.node_role_arn
+#   subnet_ids      = var.private_subnet_ids
+
+#   scaling_config {
+#     desired_size = 1
+#     max_size     = 3
+#     min_size     = 1
+#   }
+
+#   launch_template {
+#     id      = aws_launch_template.eks_node_lt.id
+#     version = "$Latest"
+#   }
+
+#   labels = {
+#     workload = "backend"
+#   }
+
+#   depends_on = [aws_eks_cluster.eks]
+# }
+
+
+resource "aws_eks_node_group" "ng" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "node_group_flask"
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.private_subnet_ids
+  instance_types  = ["t2.medium"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 3
+    min_size     = 1
+  }
+
+  labels = {
+    workload = "backend"
+  }
+
+  disk_size = 20
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [aws_eks_cluster.eks]
+}
+
+
+
+
+resource "aws_eks_fargate_profile" "frontend" {
+  cluster_name           = aws_eks_cluster.eks.name
+  fargate_profile_name   = "frontend-fargate-profile"
+  pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
+  subnet_ids             = var.private_subnet_ids
+  selector {
+    namespace = "frontend"
+  }
+
+  depends_on = [aws_eks_cluster.eks]
+}
+
+
+
+data "aws_partition" "current" {}
+
+# Resource: AWS IAM Open ID Connect Provider
+resource "aws_iam_openid_connect_provider" "oidc_provider" {
+  client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
+  thumbprint_list = [var.eks_oidc_root_ca_thumbprint]
+  url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+
+  tags = {
+    Name = "eks-irsa"
+  }
+}
+
+# Extract OIDC Provider from OIDC Provider ARN
+locals {
+  aws_iam_oidc_connect_provider_extract_from_arn = element(split("oidc-provider/", "${aws_iam_openid_connect_provider.oidc_provider.arn}"), 1)
+}
