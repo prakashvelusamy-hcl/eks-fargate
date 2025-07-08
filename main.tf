9bc99818ad0f0a627c07d478b4cdd8e86dbfb6e9
prakashvelusamy-hcl <prakashvelusamy1999@gmail.com>
2025-07-08 21:40:00 +0530

    eks-fix

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+#############################################
+# VPC Module
+#############################################
 module "vpc" {
   source         = "./modules/terraform-aws-vpc"
   vpc_cidr       = var.vpc_cidr
@@ -5,13 +8,29 @@ module "vpc" {
   priv_sub_count = var.priv_sub_count
   nat_count      = var.nat_count
 }
-# module "alb" {
-#   source = "./modules/terraform-aws-alb"
-#   public_subnet_ids = module.vpc.public_subnet_ids
-#   environment = var.environment
-#   vpc_id = module.vpc.vpc_id
-# }
 
+#############################################
+# EKS IAM
+#############################################
+module "iam" {
+  source = "./modules/terraform-aws-iam"
+  aws_iam_openid_connect_provider_arn              = module.eks.aws_iam_openid_connect_provider_arn
+  aws_iam_openid_connect_provider_extract_from_arn = module.eks.aws_iam_openid_connect_provider_extract_from_arn
+}
+
+#############################################
+# RDS
+#############################################
+module "rds" {
+  source             = "./modules/terraform-aws-rds"
+  private_subnet_ids = module.vpc.private_subnet_ids
+  vpc_id             = module.vpc.vpc_id
+  eks_node_sg_id     = module.eks.eks_node_sg_id
+}
+
+#############################################
+# EKS Module
+#############################################
 module "eks" {
   source                      = "./modules/terraform-aws-eks"
   private_subnet_ids          = module.vpc.private_subnet_ids
@@ -23,58 +42,17 @@ module "eks" {
   vpc_id                      = module.vpc.vpc_id
   credentials_secret_arn      = module.rds.credentials_secret_arn
   connection_secret_arn       = module.rds.connection_secret_arn
+
   providers = {
     kubernetes = kubernetes.eks
   }
+
   depends_on = [module.vpc]
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-# }
-
-module "rds" {
-  source             = "./modules/terraform-aws-rds"
-  private_subnet_ids = module.vpc.private_subnet_ids
-  vpc_id             = module.vpc.vpc_id
-  eks_node_sg_id     = module.eks.eks_node_sg_id
-
-}
-
-#module "apps" {
-#  source = "./modules/terraform-aws-apps"
-#}
-
-
-module "iam" {
-  source                                           = "./modules/terraform-aws-iam"
-  aws_iam_openid_connect_provider_arn              = module.eks.aws_iam_openid_connect_provider_arn
-  aws_iam_openid_connect_provider_extract_from_arn = module.eks.aws_iam_openid_connect_provider_extract_from_arn
-}
-
-
-module "helm" {
-  source                             = "./modules/terraform-aws-helm"
-  cluster_id                         = module.eks.cluster_id
-  cluster_endpoint                   = module.eks.cluster_endpoint
-  cluster_certificate_authority_data = module.eks.cluster_certificate_authority_data
-  lbc_iam_depends_on                 = module.iam.lbc_iam_depends_on
-  lbc_iam_role_arn                   = module.iam.lbc_iam_role_arn
-  vpc_id                             = module.vpc.vpc_id
-  region                             = var.region
-
-  providers = {
-    kubernetes = kubernetes.eks
-    helm       = helm.eks
-  }
-}
-
+#############################################
+# EKS Data Sources (AFTER EKS IS CREATED)
+#############################################
 data "aws_eks_cluster" "eks" {
   name = module.eks.cluster_id
 }
@@ -83,6 +61,9 @@ data "aws_eks_cluster_auth" "eks" {
   name = module.eks.cluster_id
 }
 
+#############################################
+# Kubernetes Provider (With EKS Token)
+#############################################
 provider "kubernetes" {
   alias                  = "eks"
   host                   = data.aws_eks_cluster.eks.endpoint
@@ -90,12 +71,33 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
-
+#############################################
+# Helm Provider
+#############################################
 provider "helm" {
   alias = "eks"
   kubernetes {
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
+
+#############################################
+# Helm Module
+#############################################
+module "helm" {
+  source                             = "./modules/terraform-aws-helm"
+  cluster_id                         = module.eks.cluster_id
+  cluster_endpoint                   = data.aws_eks_cluster.eks.endpoint
+  cluster_certificate_authority_data = data.aws_eks_cluster.eks.certificate_authority[0].data
+  lbc_iam_depends_on                 = module.iam.lbc_iam_depends_on
+  lbc_iam_role_arn                   = module.iam.lbc_iam_role_arn
+  vpc_id                             = module.vpc.vpc_id
+  region                             = var.region
+
+  providers = {
+    kubernetes = kubernetes.eks
+    helm       = helm.eks
   }
 }
